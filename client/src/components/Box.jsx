7abc5578191d325d7e6e1c9a589d3c2a2986ea11
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-19 18:42:11 -0400

    styled grid boxes with inline react styles

--- client/src/components/Box.jsx
+++ client/src/components/Box.jsx
@@ -1,32 +1,63 @@
 import React from 'react';
 import * as Tone from 'tone';
 
-const checkedBoxStyle = {
-  'borderStyle': 'solid',
-  'borderColor': 'black',
-  'backgroundColor': 'black',
-  'width': '100px',
-  'height': '100px',
-};
+// const boxStyle = {
+//   'borderStyle': 'solid',
+//   'borderColor': 'black',
+//   'width': '100px',
+//   'height': '100px',
+// };
 
-const boxStyle = {
-  'borderStyle': 'solid',
-  'borderColor': 'black',
-  'width': '100px',
-  'height': '100px',
+// const checkedBoxStyle = {
+//   'backgroundColor': 'black'
+// };
+
+const hoverBoxStyle = {
+ 'boxShadow': '0 0 40px #5ED4F3'
 };
 
 class Box extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      hover: false
+    }
+  };
+
+  getBoxStyle() {
+    return this.props.value === 1 ?
+    {
+      'backgroundColor': '#5ED4F3',
+      'borderStyle': 'solid',
+      'borderColor': '#EFEFEF',
+      'width': '100px',
+      'height': '100px',
+    } :
+    {
+      'borderStyle': 'solid',
+      'borderColor': '#EFEFEF',
+      'width': '100px',
+      'height': '100px',
+    };
   }
+
+  toggleHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  };
+
   render() {
     return (
       <td>
         <div
-          style={this.props.value === 1 ? checkedBoxStyle : boxStyle}
+          style={
+            this.state.hover ? {...this.getBoxStyle(), ...hoverBoxStyle} : this.getBoxStyle()
+          }
           onClick={ () => {this.props.boxToggle(this.props.row, this.props.i)} }
-        >
+          onMouseEnter={ () => this.toggleHover() }
+          onMouseLeave={ () => this.toggleHover() }>
         </div>
       </td>
     )
