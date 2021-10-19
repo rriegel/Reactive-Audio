0fc5002016bd22a056705610cbe1b5a6358439bd
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-19 16:29:32 -0400

    replaced checkboxes with div boxes and adjusted style based on clicked status

--- client/src/components/Box.jsx
+++ client/src/components/Box.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import * as Tone from 'tone';
 
+const checkedBoxStyle = {
+  'borderStyle': 'solid',
+  'borderColor': 'black',
+  'backgroundColor': 'black',
+  'width': '100px',
+  'height': '100px',
+};
+
+const boxStyle = {
+  'borderStyle': 'solid',
+  'borderColor': 'black',
+  'width': '100px',
+  'height': '100px',
+};
+
 class Box extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return(
-      <td><input type='checkbox' checked={this.props.value} onChange={ () => {this.props.boxToggle(this.props.row, this.props.i)} }/></td>
+    return (
+      <td>
+        <div
+          style={this.props.value === 1 ? checkedBoxStyle : boxStyle}
+          onClick={ () => {this.props.boxToggle(this.props.row, this.props.i)} }
+        >
+        </div>
+      </td>
     )
   }
 };
