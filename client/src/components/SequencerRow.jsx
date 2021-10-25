1ab50498b05d215ca5365b798a990fe807e9b07e
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-25 16:15:39 -0400

    refactored to functional component with props destructuring

--- client/src/components/SequencerRow.jsx
+++ client/src/components/SequencerRow.jsx
@@ -2,22 +2,16 @@ import React from 'react';
 import Box from './Box.jsx';
 import * as Tone from 'tone';
 
-class SequencerRow extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <tr>
-        {this.props.checked[this.props.row].map((boxValue, key) => {
-          return (
-            <Box key={key} i={key} value={boxValue} row={this.props.row} boxToggle={ this.props.boxToggle } />
-          )
-        })}
-      </tr>
-    )
-  }
+function SequencerRow({checked, row, boxToggle}) {
+  return (
+    <tr>
+      {checked[row].map((boxValue, key) => {
+        return (
+          <Box key={ key } i={ key } value={ boxValue } row={ row } boxToggle={ boxToggle } />
+        )
+      })}
+    </tr>
+  )
 };
 
 export default SequencerRow;
