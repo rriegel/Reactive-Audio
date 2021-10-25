38a28a3f3d20315cc3061807d1970acd183a5666
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-25 16:14:03 -0400

    minor spacing adjustments

--- client/src/components/Box.jsx
+++ client/src/components/Box.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Box.css';
 import * as Tone from 'tone';
 
-function Box ({ i, value, row, boxToggle }) {
+function Box({ i, value, row, boxToggle }) {
   return (
     <td>
       <div
         className={ value === 1 ? "box clicked" : "box" }
-        onClick={ () => {boxToggle(row, i)} }>
+        onClick={ () => { boxToggle(row, i) } }>
       </div>
     </td>
   )
