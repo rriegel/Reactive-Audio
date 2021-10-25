9972a34187ea15afc2d47ffd34634da5d1206837
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-25 14:00:20 -0400

    added css-loader and style-loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,13 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   }
