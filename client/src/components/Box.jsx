7e3da93833ed8563432c26176a6fe35a79daae9e
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-25 15:47:51 -0400

    extended full usage of destructured props

--- client/src/components/Box.jsx
+++ client/src/components/Box.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Box.css';
 import * as Tone from 'tone';
 
-function Box ({ value, boxToggle }) {
+function Box ({ i, value, row, boxToggle }) {
   return (
     <td>
       <div
         className={ value === 1 ? "box clicked" : "box" }
-        onClick={ () => {boxToggle(this.props.row, this.props.i)} }>
+        onClick={ () => {boxToggle(row, i)} }>
       </div>
     </td>
   )
