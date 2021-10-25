2c12c6e7f50b183913492ac257fcc78228bf3c08
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-25 15:41:44 -0400

    added Box.css to support :hover with css and add box-shadow transition AND refactored Box.jsx to be a function component, much cleaner now

--- client/src/components/Box.jsx
+++ client/src/components/Box.jsx
@@ -1,54 +1,16 @@
 import React from 'react';
+import './Box.css';
 import * as Tone from 'tone';
 
-const hoverBoxStyle = {
- 'boxShadow': '0 0 40px #5ED4F3'
-};
-
-class Box extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hover: false
-    }
-  };
-
-  getBoxStyle() {
-    return this.props.value === 1 ?
-    {
-      'backgroundColor': '#5ED4F3',
-      'borderStyle': 'solid',
-      'borderColor': '#EFEFEF',
-      'width': '100px',
-      'height': '100px',
-    } :
-    {
-      'borderStyle': 'solid',
-      'borderColor': '#EFEFEF',
-      'width': '100px',
-      'height': '100px',
-    };
-  }
-
-  toggleHover() {
-    this.setState({
-      hover: !this.state.hover
-    });
-  };
-
-  render() {
-    return (
-      <td>
-        <div
-          style={ this.state.hover ? {...this.getBoxStyle(), ...hoverBoxStyle} : this.getBoxStyle() }
-          onClick={ () => {this.props.boxToggle(this.props.row, this.props.i)} }
-          onMouseEnter={ () => this.toggleHover() }
-          onMouseLeave={ () => this.toggleHover() }>
-        </div>
-      </td>
-    )
-  }
-};
+function Box ({ value, boxToggle }) {
+  return (
+    <td>
+      <div
+        className={ value === 1 ? "box clicked" : "box" }
+        onClick={ () => {boxToggle(this.props.row, this.props.i)} }>
+      </div>
+    </td>
+  )
+}
 
 export default Box;
