e66e8f73f67c596819b379a05485b51eb159f606
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-25 14:16:19 -0400

    moved styles.css and started to reconfigure css-loader and styles-loader

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../public/styles.css';
 import App from './app.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,8 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
-          'css-loader'
+          'css-loader',
+          'style-loader'
         ]
       }
     ]
