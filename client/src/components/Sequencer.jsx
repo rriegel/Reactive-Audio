4af37ed20bb8fbd96ae8120e86be0a50a353ae27
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-26 12:38:25 -0400

    refactored Sequencer to a function component with hooks

--- client/src/components/Sequencer.jsx
+++ client/src/components/Sequencer.jsx
@@ -1,83 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as Tone from 'tone';
 import SequencerRow from './SequencerRow.jsx';
 import StartAudioContext from "startaudiocontext";
 import sampler from './utils/sampler.js';
 
-class Sequencer extends React.Component {
-  constructor(props) {
-    super(props);
+function Sequencer({ notes, octaves, BPM, isPlaying, checked, boxToggle }) {
 
-    this.state = { isLoaded: false }
-
-  }
-
-  togglePlay() {
+  const togglePlay = () => {
     if (Tone.Transport.state === 'stopped') {
 
       const repeat = (time) => {
         let note = row0[index % row0.length];
         // trigger sampler to conditionally play note
-        row0[index] === 1 ? sampler.triggerAttackRelease(`${this.props.notes[0]}${this.props.octaves[0]}`, '8n', time) : null;
-        row1[index] === 1 ? sampler.triggerAttackRelease(`${this.props.notes[1]}${this.props.octaves[1]}`, '8n', time) : null;
-        row2[index] === 1 ? sampler.triggerAttackRelease(`${this.props.notes[2]}${this.props.octaves[2]}`, '8n', time) : null;
-        row3[index] === 1 ? sampler.triggerAttackRelease(`${this.props.notes[3]}${this.props.octaves[3]}`, '8n', time) : null;
+        row0[index] === 1 ? sampler.triggerAttackRelease(`${notes[0]}${octaves[0]}`, '8n', time) : null;
+        row1[index] === 1 ? sampler.triggerAttackRelease(`${notes[1]}${octaves[1]}`, '8n', time) : null;
+        row2[index] === 1 ? sampler.triggerAttackRelease(`${notes[2]}${octaves[2]}`, '8n', time) : null;
+        row3[index] === 1 ? sampler.triggerAttackRelease(`${notes[3]}${octaves[3]}`, '8n', time) : null;
         // make sure index stays within bounds
         if (index === row0.length - 1) index = 0
         else index ++;
       };
 
-      const row0 = this.props.checked[0];
-      const row1 = this.props.checked[1];
-      const row2 = this.props.checked[2];
-      const row3 = this.props.checked[3];
+      const row0 = checked[0];
+      const row1 = checked[1];
+      const row2 = checked[2];
+      const row3 = checked[3];
 
       let speed = '8n';
       let index = 0;
       Tone.Transport.scheduleRepeat(time => { repeat(time) }, speed);
-
-      Tone.Transport.bpm.value = this.props.BPM;
+      // BPM is determined by parent state
+      Tone.Transport.bpm.value = BPM;
 
       Tone.Transport.start();
     } else {
       Tone.Transport.cancel();
       Tone.Transport.stop();
     }
-  }
+  };
 
-  componentDidUpdate(prevProps) {
-    if (this.props.isPlaying !== prevProps.isPlaying) {
-      this.togglePlay();
+  const usePrevious = (value) => {
+    const ref = useRef();
+    useEffect(() => ref.current = value);
+    return ref.current;
+  };
+  const prevPlaying = usePrevious(isPlaying);
+  useEffect(() => {
+    if (prevPlaying !== undefined && isPlaying !== prevPlaying) {
+      togglePlay();
     }
-  }
-  render() {
-    return(
-      <table className='sequencer'>
-        <tbody>
-          <SequencerRow
-            checked={ this.props.checked }
-            row='0'
-            boxToggle={ this.props.boxToggle }
-          />
-          <SequencerRow
-            checked={ this.props.checked }
-            row='1'
-            boxToggle={ this.props.boxToggle }
-          />
-          <SequencerRow
-            checked={ this.props.checked }
-            row='2'
-            boxToggle={ this.props.boxToggle }
-          />
-          <SequencerRow
-            checked={ this.props.checked }
-            row='3'
-            boxToggle={ this.props.boxToggle }
-          />
-        </tbody>
-      </table>
-    )
-  }
+  }, [isPlaying]);
+
+  return (
+    <table className='sequencer'>
+      <tbody>
+        <SequencerRow
+          checked={ checked }
+          row='0'
+          boxToggle={ boxToggle }
+        />
+        <SequencerRow
+          checked={ checked }
+          row='1'
+          boxToggle={ boxToggle }
+        />
+        <SequencerRow
+          checked={ checked }
+          row='2'
+          boxToggle={ boxToggle }
+        />
+        <SequencerRow
+          checked={ checked }
+          row='3'
+          boxToggle={ boxToggle }
+        />
+      </tbody>
+    </table>
+  )
 };
 
 export default Sequencer;
