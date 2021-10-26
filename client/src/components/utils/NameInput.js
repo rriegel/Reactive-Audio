72b2ec1ee8b8ef87784a7bd3244c663aa009c138
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-26 18:38:13 -0400

    adjusted attributes on NameInput to respond to change and enter keyPress, as well as give error messages when name is empty or name matches a previous name

--- client/src/components/utils/NameInput.js
+++ client/src/components/utils/NameInput.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField';
 
 export default function NameInput({ projectList }) {
 
-  // const error = (name) => {
-  //   console.log(name);
-  // }
-  // const helper = (name) => {
-  //   if (!name.length) {
-  //     return 'Name cannot be empty';
-  //   }
-  //   if (projectList.indexOf(name) !== -1) {
-  //     return `${name} already exists`;
-  //   }
-  // }
+  const [name, setName] = useState(null);
+  const [touched, setTouched] = useState(false);
 
+  const error = name ? false : true;
+
+  const helper = () => {
+    if (!name) {
+      return 'Name your project';
+    }
+    if (projectList.indexOf(name) !== -1) {
+      return `${name} already exists`;
+    }
+  };
+
+  useEffect(() => { helper() }, [name]);
+
+  const handleTouch = () => setTouched(true);
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  }
   const keyPress = (e) => {
     if (e.keyCode === 13) {
       e.preventDefault();
       console.log('value', e.target.value);
     }
- }
+  }
 
   return (
     <Box
@@ -37,9 +46,11 @@ export default function NameInput({ projectList }) {
         <TextField
           id="name-input"
           placeholder="Project Name"
-          onKeyDown={(e) => keyPress(e)}
-          // helperText={helper()}
-          // error={error(name)}
+          onFocus={handleTouch}
+          onChange={handleChange}
+          onKeyDown={keyPress}
+          error={ touched && error }
+          helperText={ touched && helper() }
         />
       </div>
     </Box>
