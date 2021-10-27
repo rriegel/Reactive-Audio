4762e3b1f3fb264ccecde2977fd016872cdc829b
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-27 13:49:05 -0400

    created sequencer css sheet to begin styling the sequencer background and adjusted GridBox.css to add a default background color

--- client/src/components/Sequencer.jsx
+++ client/src/components/Sequencer.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import './Sequencer.css';
 import SequencerRow from './SequencerRow.jsx';
 import togglePlay from './utils/togglePlay.js';
 
