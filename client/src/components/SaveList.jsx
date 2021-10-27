7c58a6e80cd164dcc7f4096a4d3f2976be069e76
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-27 12:04:25 -0400

    refactored SaveList.jsx from class-based to functional component

--- client/src/components/SaveList.jsx
+++ client/src/components/SaveList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SaveState from './SaveState.jsx';
-
+/*
 class SaveList extends React.Component {
   constructor(props) {
     super(props);
@@ -8,11 +8,13 @@ class SaveList extends React.Component {
   renderList() {
     return (
       <div className='save-list-container'>
-      {this.props.saves.map((save, key) => {
-        return (
-          < SaveState key={key} name={save} loadSave={this.props.loadSave} deleteSave={this.props.deleteSave}/>
-        )
-      })}
+      {this.props.saves.map((save, key) => (
+        < SaveState
+          key={ key }
+          name={ save }
+          loadSave={ this.props.loadSave }
+          deleteSave={ this.props.deleteSave }/>
+      ))}
       </div>
     )
   }
@@ -32,7 +34,40 @@ class SaveList extends React.Component {
         </div>
       )
     }
+  }
+};
+*/
 
+function SaveList({ saves, loadSave, deleteSave }) {
+
+  const renderList = () => {
+    return (
+      <div className='save-list-container'>
+      {saves.map((save, key) => (
+        < SaveState
+          key={ key }
+          name={ save }
+          loadSave={ loadSave }
+          deleteSave={ deleteSave }/>
+      ))}
+      </div>
+    )
+  };
+
+  if (saves.length > 0) {
+    return (
+      <div>
+        <h2>Saved Projects</h2>
+        { renderList() }
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>Saved Projects</h2>
+        <h4>no saved projects</h4>
+      </div>
+    )
   }
 };
 
