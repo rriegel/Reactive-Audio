ebbfa2ae1054a80b81e5eec08dbafbd9a5c80521
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-27 12:05:29 -0400

    refactored SaveList.jsx from class-based to functional component

--- client/src/components/SaveList.jsx
+++ client/src/components/SaveList.jsx
@@ -1,44 +1,7 @@
 import React from 'react';
 import SaveState from './SaveState.jsx';
-/*
-class SaveList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  renderList() {
-    return (
-      <div className='save-list-container'>
-      {this.props.saves.map((save, key) => (
-        < SaveState
-          key={ key }
-          name={ save }
-          loadSave={ this.props.loadSave }
-          deleteSave={ this.props.deleteSave }/>
-      ))}
-      </div>
-    )
-  }
-  render() {
-    if (this.props.saves.length > 0) {
-      return (
-        <div>
-          <h2>Saved Projects</h2>
-          { this.renderList() }
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <h2>Saved Projects</h2>
-          <h4>no saved projects</h4>
-        </div>
-      )
-    }
-  }
-};
-*/
 
-function SaveList({ saves, loadSave, deleteSave }) {
+export default function SaveList({ saves, loadSave, deleteSave }) {
 
   const renderList = () => {
     return (
@@ -70,5 +33,3 @@ function SaveList({ saves, loadSave, deleteSave }) {
     )
   }
 };
-
-export default SaveList;
