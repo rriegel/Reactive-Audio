ffd05c5ebc51fa1cc01536384657691837669084
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-27 12:08:21 -0400

    refactored SaveState to functional component

--- client/src/components/SaveState.jsx
+++ client/src/components/SaveState.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-class SaveState extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className='save-state-entry'>
-        <h4>{this.props.name}</h4>
-        <button onClick={() => {this.props.loadSave(this.props.name)}}>Load</button>
-        <button onClick={() => {this.props.deleteSave(this.props.name)}}>Delete</button>
-      </div>
-    )
-  }
+export default function SaveState({ name, loadSave, deleteSave }) {
+  return (
+    <div className='save-state-entry'>
+      <h4>{ name }</h4>
+      <button onClick={ () => loadSave(name) }>Load</button>
+      <button onClick={ () => deleteSave(name) }>Delete</button>
+    </div>
+  )
 };
-
-export default SaveState;
